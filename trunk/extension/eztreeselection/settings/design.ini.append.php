5d0dafe6cebc7d7c7a9f2ae420b884d027392afb
nfrp <nfrp@ez.no>
2008-08-01 09:51:03 +0000

    --it is now possible to have several eztreeselection attributes on the same page
    git-svn-id: http://svn.projects.ez.no/eztreeselection@7 ddb2af1a-6211-4f3a-bdb3-11739b1a15cd

--- trunk/extension/eztreeselection/settings/design.ini.append.php
+++ trunk/extension/eztreeselection/settings/design.ini.append.php
@@ -6,4 +6,10 @@ DesignExtensions[]=eztreeselection
 [StylesheetSettings]
 CSSFileList[]=treeview/assets/skins/ez/treeview.css
 
+[JavaScriptSettings]
+JavaScriptList[]=yahoo/yahoo-min.js
+JavaScriptList[]=event/event-min.js
+JavaScriptList[]=treeview/treeview-min.js
+JavaScriptList[]=eztreeselection.js
+
 */ ?>
